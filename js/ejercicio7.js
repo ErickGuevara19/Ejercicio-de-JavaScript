7f790fa47876cda1ae0a01ecf1911d96823bb466
Erick <ead.guevara@yavirac.edu.ec>
2022-02-23 11:44:49 -0500

    Ejercicios de Js

---
+++ js/ejercicio7.js
@@ -0,0 +1,61 @@
+let letra
+
+alert("Programa que determine si una letra es consonante o volcal");
+
+letra =  prompt("Escriba la letra que desea evaluar");
+document.getElementById("letra").innerHTML = "La letra ingresada es : " + letra;
+
+if (letra = A){
+
+     alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = a){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+} else if (letra = E){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = e){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = I){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = i){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = O){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = o){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = U){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+
+}else if (letra = u){
+
+    alert("Es una vocal");
+    document.getElementById("frase").innerHTML = "Es una vocal";
+}else {
+
+    alert("No es una vocal");
+}
+
---
+++ js/ejercicio9.js
@@ -0,0 +1,6 @@
+let nota 
+let nombre
+
+alert("Programa para separar a los alumnos dependiendo la nota obtenida")
+
+nombre = prompt("")
